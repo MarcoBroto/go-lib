2caecd2b116a8da9bec8c5989128fd8b595841da
Marco Soto <msoto@Marcos-M3-Max-MacBook-Pro.local>
2025-05-22 00:18:03 -0400

    Unit test skeleton

---
+++ src/rb_tree/rb_tree_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateRbTree(t *testing.T) {
+}
+
+func TestIsValidRbTreeReturnsTrue(t *testing.T) {
+
+}
+
+func TestIsValidRbTreeReturnsFalse(t *testing.T) {
+
+}
+
+func TestIsValidRbTreeReturnsPanics(t *testing.T) {
+}
+
+func TestInsert(t *testing.T) {
+}
+
+func TestInsertRandom(t *testing.T) {
+	assert.True(t, true)
+}
+
+func TestRemove(t *testing.T) {
+	assert.True(t, true)
+}
+
+func TestRotate_LL(t *testing.T) {
+	assert.True(t, true)
+}
+
+func TestLRRotate_LR(t *testing.T) {
+	assert.True(t, true)
+}
+
+func TestRotate_RR(t *testing.T) {
+	assert.True(t, true)
+}
+
+func TestRotate_RL(t *testing.T) {
+	assert.True(t, true)
+}
+
+func TestRecolor(t *testing.T) {
+	assert.True(t, true)
+}
